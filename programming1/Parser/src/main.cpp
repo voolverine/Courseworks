ec9ce12a830cd655a86707a9215eb9a8b079dc7c
Denis Svidunovich <volverinemail@gmail.com>
2015-06-05 21:45:10 +0300

    Added check for multiply files. Finalize.

--- programming1/Parser/src/main.cpp
+++ programming1/Parser/src/main.cpp
@@ -44,6 +44,11 @@ FILE *articles = NULL;
 
 pair<long long, long long> getHash(string s) 
 {
+    while (s.size() > 0 && s[(int)s.size() - 1] == '\n') 
+    {
+        s = s.substr(0, (int)s.size() - 1);
+    }
+
     long long hash1 = 0;
     long long hash2 = 0;
     long long power1 = 1;
@@ -72,7 +77,11 @@ vector<string> get_filenames_in_dir(string dir)
     while (!feof(names) && fgets(buff, 1000, names)) 
     {
         string new_file = buff;
-        new_file.erase(new_file.end() - 1, new_file.end());     // remove \n at the end
+        while (new_file.size() > 0 && new_file[(int)new_file.size() - 1] == '\n') 
+        {
+            new_file = new_file.substr(0, (int)new_file.size() - 1);
+        }
+
         result.push_back(new_file);
     }
 
@@ -114,8 +123,14 @@ void Parse_all_files()
     vector<string> all_files = get_filenames_in_dir(html_dir);
 
     for (int i = 0; i < (int)all_files.size(); i++) 
-    {
+    {   
+        if (all_files[i] == "." || all_files[i] == "..") 
+        {
+            continue;
+        }
+        
         pair<string, string> text = parse(html_dir + all_files[i]);
+
         if (!already_saved(get_url_from_title(text.first))) 
         {
             save_text(text.first, text.second, get_url_from_title(all_files[i]));
@@ -126,7 +141,8 @@ void Parse_all_files()
             break;
         }
     }
-
+    
+    printf("Parsing finished. Press Enter.\n");
     return;
 }
 
@@ -143,7 +159,7 @@ void read_info()
     long int from, to;
     char buff[200];
 
-    while (fscanf(id, "%ld %ld %s\n", &from, &to, buff) != EOF) 
+    while (fscanf(id, "%ld %ld %s", &from, &to, buff) != EOF) 
     {
         if (!already_saved(buff)) 
         {
@@ -169,6 +185,21 @@ void save_info()
 }
 
 
+void remove_all_copies(string dir) 
+{
+    vector<string> all_files = get_filenames_in_dir(dir);
+
+    for (int i = 0; i < (int)all_files.size(); i++) 
+    {
+        pair<string, string> file = parse(html_dir + all_files[i]);
+        file.first = get_url_from_title(file.first);
+        system(("mv \"" + html_dir + all_files[i] + "\" \"" + html_dir + file.first.c_str() + "\"").c_str());
+    }
+
+}
+
+
+
 int main(int argc, char *argv[]) 
 {
     if (argc == 2) 
@@ -177,13 +208,14 @@ int main(int argc, char *argv[])
         text_dir = argv[1];
     } else 
     {
-        html_dir = "../html_files/";
-        text_dir = "../text_files/";
+        html_dir = "../../html_files/";
+        text_dir = "../../text_files/";
     }
 
     articles = fopen((text_dir + "articles.txt").c_str(), "a+");
     
     read_info();
+    //remove_all_copies(html_dir);
     thread Parser(Parse_all_files); 
 
     char c;
